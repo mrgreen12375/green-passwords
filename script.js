792a4fafea7af472cc9ed81d7e1dbf209a435652
Steven Green <ste2144105@gmail.com>
2022-08-17 21:18:32 -0700

    created if statements to store character options

--- script.js
+++ script.js
@@ -32,7 +32,26 @@ function generatePassword() {
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var storedOptions = [];
-  
+
+  if (confirmSpecialCharacters) {
+    storedOptions.push(specialCharacters);
+  }
+
+  if (confirmNumbers) {
+    storedOptions.push(numbers);
+  }
+
+  if (confirmLowerCase) {
+    storedOptions.push(lowerCase);
+  }
+
+  if (confirmUpperCase) {
+    storedOptions.push(upperCase);
+  }
+
+  if (storedOptions.length === 0) {
+    storedOptions.push(specialCharacters);
+  }
 
 }
 
