c42aab3d37fbe64cb49cd85b3dfbe855836e5193
Steven Green <ste2144105@gmail.com>
2022-08-17 17:42:12 -0700

    created initial and character length promts

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   
-  window.prompt("How many characters would you like your password to contain?")
+  var passwordInput = window.prompt("How many characters would you like your password to contain?");
+  var characterLength = parseInt(passwordInput);
+
+  if (isNaN(characterLength)) {
+    window.alert("Please enter a NUMBER first!");
+    return
+  } else {
+    console.log("valid number");
+  }
+
+  if (characterLength < 8) {
+    window.alert("Password must be at least 8 characters and no more that 128 characters.");
+    return
+  } else if (characterLength > 128) {
+    window.alert("Password must be at least 8 characters and no more that 128 characters.");
+    return
+  } else {
+    console.log("valid character length");
+  }
 
 }
 
